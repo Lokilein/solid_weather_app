89d4737ca88b7d9ab10fb65bece3b04220b64853
Lokilein <lokilein.sb@googlemail.com>
2025-12-09 20:12:03 +0100

    Prepare In/Output

--- src/Infrastructure/CLI/EntryPoint.php
+++ src/Infrastructure/CLI/EntryPoint.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Lokil\SolidWeatherApp\Infrastructure\CLI;
@@ -8,6 +9,16 @@ class EntryPoint
     public function do(): void
     {
         $cliTools = new CliTools();
-        $cliTools->writeLineBreak("Hello World!");
+        $cliTools->writeLineBreak('Von welcher Stadt soll das Wetter angezeigt werden?');
+        $city = trim($cliTools->readLine());
+        $output = strtr(
+            'Das Wetter in der Stadt %city: %weather. Es ist %temperature',
+            [
+                '%city' => $city,
+                '%weather' => 'A',
+                '%temperature' => 'B'
+            ]
+        );
+        $cliTools->writeLineBreak($output);
     }
 }
